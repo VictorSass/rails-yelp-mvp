4945706040389283736c4b3382f0e851729d76d5
Victor Sassonia <victor.sassonia@gmail.com>
2018-02-14 16:23:53 +0100

    yelps with render show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, only: [ :index, :show, :new, :create ] do
+      resources :reviews, only: [ :create, :new ]
+    end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
